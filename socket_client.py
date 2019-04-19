da16703f829505f4dab5db3fdbd8266332135361
tarekYU1993 <49770825+tarekYU1993@users.noreply.github.com>
2019-04-19 09:26:34 +0800

    Add files via upload

---
+++ socket_client.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+from socket import *
+from time import ctime
+
+Host = '10.64.165.39'
+Port = 7900
+Buffsize = 1024
+Addr = (Host,Port)
+
+client_socket = socket(AF_INET,SOCK_STREAM)
+client_socket.connect(Addr)
+
+while True:
+	data = raw_input('input data:')
+	if not data:
+		break
+	client_socket.send(data)
+	data = client_socket.recv(Buffsize)
+	if not data:
+		break
+	print data
